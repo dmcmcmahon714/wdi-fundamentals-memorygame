30db2ed08ae4d83d39de9a41af0744c4fd38e214
Daniel McMahon <danielmcmahon@Daniels-MacBook-Pro.local>
2019-11-25 15:44:30 -0800

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,13 +1,22 @@
+console.log ("Up and running")
 var cards = ['queen', 'queen', 'king', 'king'];
-var cardsInPlay = [];
 
-var cardOne = cards[0];
-cardsInPlay.push(cardOne);
-console.log("User flipped queen.");
-var cardTwo = cards[2];
-cardsInPlay.push(cardTwo);
-console.log("User flipped king.");
-if (cardsInPlay.length === 2) {}
-if else (cardsinPlay[0] === cardsinPlay[1]) {alert("You found a match!")}
-else {alert("Sorry, try again.")}
+var cardsInPlay = [];
+function checkForMatch() {
+	if(cardsInPlay[0] === cardsInPlay[1]) {
+		console.log("You found a match!");
+	} else {
+		console.log("Sorry, try again.")
+	}
+};
+ 
+function flipCard() {
+	var cardId = this.getAtrribute ('data-id');
+	console.log("User flipped" + card[cardID].rank);
+	cardsInPlay.push(cards[cardID].rank);
+	this.setAtrribute('src',cards[cardID].cardImage);
+	checkForMatch();
+	console.log(cards[cardId].cardImage);
+	console.log(cards[cardId.suit]);
+}
 
